b848cc90548800a7f2d704a3b80c8253c0426d38
jasonlikespi <108479538+jasonlikespi@users.noreply.github.com>
2022-07-07 09:22:19 -0700

    commit

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -6,5 +6,8 @@ public class MarkdownParseTest {
     public void addition() {
         assertEquals(2, 1 + 1);
     }
-
+    @Test
+    public void test-file(){
+        assertEquals(getLinks(test-file.md), {"https://something.com", "some-page.html"}); 
+    }
 }
