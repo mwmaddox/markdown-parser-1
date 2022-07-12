7e87cb5241505c5e9793fdab4cd4441051759b96
mwmaddox <108300270+mwmaddox@users.noreply.github.com>
2022-07-12 08:40:05 -0700

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -7,4 +7,28 @@ public class MarkdownParseTest {
         assertEquals(2, 1 + 1);
     }
 
+    @Test
+    public void testLinkAtBeginning() {
+        String contents= "[link title](a.com)";
+        List<String> expect = List.of("a.com");
+        assertEquals(MarkdownParse.getLinks(contents), expect);
+    }
+
+    @Test
+    public void testInfiniteLoop() { //failed and got stuck in infinite loop
+        String contents = ")[";
+        List<String> expect = new ArrayList<String>();
+        assertEquals(expect, MarkdownParse.getLinks(contents));
+    }
+
+    @Test
+    public void testRegular() {
+        String contents = "# Title [link1](https://something.com)[link2](some-thing.html)";
+
+        List<String> expect = new ArrayList<String>();
+        expect.add("https://something.com");
+        expect.add("some-thing.html");
+        assertEquals(expect, MarkdownParse.getLinks(contents));
+    }
+
 }
