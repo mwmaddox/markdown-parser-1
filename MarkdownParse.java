7b801db6673721cc581f344d7311b1df28c89132
mwmaddox <108300270+mwmaddox@users.noreply.github.com>
2022-07-12 08:31:24 -0700

    Update MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -12,6 +12,12 @@ public class MarkdownParse {
         // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
+		if (!(markdown.contains("[") && markdown.contains("]"))) {
+                	return toReturn;
+            }
+            if (!(markdown.contains("(") && markdown.contains(")"))) {
+                	return toReturn;
+            }
             int openBracket = markdown.indexOf("[", currentIndex);
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", closeBracket);
